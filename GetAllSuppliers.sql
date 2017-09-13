0ac6765db62572ca0eb016670c88f8b3400c0d8b
umar-qureshi2 <umar.mcs@gmail.com>
2017-09-13 12:41:12 +0400

    supplier fo specific product

--- GetAllSuppliers.sql
+++ GetAllSuppliers.sql
@@ -10,6 +10,7 @@ AS
 BEGIN
 Select * from Suppliers --where SupplierID = @SupplierID
 END
+GO
 
 DROP PROCEDURE IF EXISTS SupplierByID
 GO
@@ -20,13 +21,18 @@ AS
 BEGIN
 Select * from Suppliers where SupplierID = @SupplierID
 END
+GO
 
 DROP PROCEDURE IF EXISTS ProductsWithSupplier
 GO
 
 CREATE PROCEDURE ProductsWithSupplier
---@SupplierID int
+@pid int
 AS
 BEGIN
-select Suppliers.ContactName + ' supplies ' + Products.ProductName from Products inner join Suppliers on Products.SupplierID = Suppliers.SupplierID
+select Suppliers.ContactName + ' supplies ' + Products.ProductName AS SupplyInfo 
+from Products inner join Suppliers 
+on Products.SupplierID = Suppliers.SupplierID
+and Products.ProductID = @pid
 END
+GO
